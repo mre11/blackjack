e06e7a39889449fb07bf3704a562a41092bc617f
Morgan Empey <morgan.empey@gmail.com>
2017-01-11 19:29:47 -0700

    Initial support for unit testing using gtest.

---
+++ utBlackjack/utCard.cpp
@@ -0,0 +1,25 @@
+#include "Card.h"
+
+#include "gtest/gtest.h"
+
+TEST(TestCard, DefaultConstructor)
+{
+	Card c;
+
+	EXPECT_EQ(0, c.getRank());
+	EXPECT_EQ(3, c.getSuit());
+	EXPECT_EQ(1, c.getPointValue());
+	EXPECT_EQ("A", c.getRankString());
+	EXPECT_EQ("S", c.getSuitString());
+}
+
+TEST(TestCard, SuitRankConstructor)
+{
+	Card c(1, 3);
+
+	EXPECT_EQ(3, c.getRank());
+	EXPECT_EQ(1, c.getSuit());
+	EXPECT_EQ(4, c.getPointValue());
+	EXPECT_EQ("4", c.getRankString());
+	EXPECT_EQ("D", c.getSuitString());
+}

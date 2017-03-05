e1fd425a293b1d500a603287121fb7277bc464c0
Morgan Empey <morgan.empey@gmail.com>
2017-03-05 10:55:43 -0700

    Run unit tests after Release or Debug build.

--- testAll.bat
+++ testAll.bat
@@ -1,2 +1,4 @@
-%~dp0bin\x64\Debug\utBlackjack.exe
-pause
+set config=Release
+if "%1"=="-d" (set config=Debug)
+echo Running unit tests for %config% configuration
+%~dp0bin\x64\%config%\utBlackjack.exe

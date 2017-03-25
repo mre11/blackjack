e1979d6e84f4a2bfaa2c011a7780a8376b0bf578
Morgan Empey <morgan.empey@gmail.com>
2017-03-25 12:34:33 -0600

    Fix testAll.bat to allow spaces in path

--- testAll.bat
+++ testAll.bat
@@ -1,4 +1,4 @@
 set config=Release
 if "%1"=="-d" (set config=Debug)
 echo Running unit tests for %config% configuration
-%~dp0bin\x64\%config%\utBlackjack.exe
+"%~dp0"bin\x64\%config%\utBlackjack.exe
